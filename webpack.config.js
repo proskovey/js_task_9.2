2318cef6d9a510ca355908ed584807187ae7ffdc
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 12:42:40 +0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
   },
   module: {
     rules: [
