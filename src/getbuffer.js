1d4bc40f6ef5a24f833c1d8e3dfe05ffb5a399d8
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 12:28:20 +0500

    Update getbuffer.js

--- src/getbuffer.js
+++ src/getbuffer.js
@@ -3,7 +3,7 @@ export default function getBuffer() {
   return ((input) => {
     const buffer = new ArrayBuffer(data.length * 2);
     const bufferView = new Uint16Array(buffer);
-    for (let i = 0; i < input.length; i++) {
+    for (let i = 0; i < input.length; i += 1) {
       bufferView[i] = input.charCodeAt(i);
     }
     return buffer;
